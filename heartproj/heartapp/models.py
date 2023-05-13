7dbd68d941d0e40362b39c5d6c232c3231969538
sayuj <63945817+webprogrammer-dot@users.noreply.github.com>
2023-05-13 22:21:59 +0530

    Update models.py
    the database addition

--- heartproj/heartapp/models.py
+++ heartproj/heartapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class ValueStore(models.Model):
+    #GENDER_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    age=models.IntegerField()
+    gender = models.IntegerField()
+    cp=models.IntegerField()
+    trestbps=models.IntegerField()
+    cholestrol=models.IntegerField()
+    fbs=models.IntegerField()
+    restecg=models.IntegerField()
+    thalach=models.IntegerField()
+    exang=models.IntegerField()
+    oldpeak=models.IntegerField()
+    slope=models.IntegerField()
+    ca=models.IntegerField()
+    thal=models.IntegerField()
+    
+    def __str__(self):
+        return str(self.id)
+    
+    
