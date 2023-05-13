e2b18efeef88cdd9d96722bcbd39e9c354004e4e
sayuj <63945817+webprogrammer-dot@users.noreply.github.com>
2023-05-13 22:24:28 +0530

    Update views.py
    changed the predict function to predict the result

--- heartproj/heartapp/views.py
+++ heartproj/heartapp/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib import messages
-<<<<<<< HEAD
-=======
+from .models import ValueStore
+import numpy as np
+import joblib
+import pickle
 
->>>>>>> fc13a816d190ddfdfd126dd66cb1ec2d573f6954
+# with open('heartproj/decision_model.sav', 'rb') as f:
+#                 model = pickle.load(f)
 
 # Create your views here.
 def homepage1(request):
@@ -15,7 +18,37 @@ def homepage2(request):
     return render(request, 'homepage2.html')
 
 def predict(request):
+    if request.method=='POST':
+        age=int(request.POST["age"])
+        gender=int(request.POST["gender"])
+        cp=int(request.POST["cp"])
+        trestbps=int(request.POST["trestbps"])
+        cholestrol=int(request.POST["cholestrol"])
+        fbs=int(request.POST["fbs"])
+        restecg=int(request.POST["restecg"])
+        thalach=int(request.POST["thalach"])
+        exang=int(request.POST["exang"])
+        oldpeak=int(request.POST["oldpeak"])
+        slope=int(request.POST["slope"])
+        ca=int(request.POST["ca"])
+        thal=int(request.POST["thal"])
+        
+        data = np.array([[age, gender, cp, trestbps, cholestrol, fbs, restecg, thalach]]) #exang, oldpeak, slope, ca, thal
+        #arr = np.array(data)
+        # load the trained model
+        model = joblib.load("savedmodels/dt_model.pkl")
+        # model = pickle.load(open('savedmodels/decision_model.sav','rb'))
+        
+        # predict the output for the input values
+        prediction =  model.predict(data)
+        
+        ValueStore.objects.create(age=age,gender=gender,cp=cp,trestbps=trestbps,cholestrol=cholestrol,fbs=fbs,restecg=restecg,thalach=thalach,exang=exang,oldpeak=oldpeak,slope=slope,ca=ca,thal=thal)
+        
+        return render(request, 'predict.html', {'prediction': prediction})
+    
     return render(request, 'predict.html')
+
+
 def loginn(request):
     if request.method=='POST':
         username=request.POST.get('username')
@@ -23,7 +56,7 @@ def loginn(request):
         user=authenticate(request,username=username,password=pass1)
         if user is not None:
             messages.info(request,"you logged in ")
-            #return redirect('sup')
+            return redirect('home2')
             #login(request,user)
             #return redirect('sin')
         else:
